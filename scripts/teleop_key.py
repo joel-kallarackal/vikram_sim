4843275a9ea80a64cb2c75637b2d46dac6c89dca
Joel <joelgeorgekal@gmail.com>
2023-07-08 20:16:31 +0530

    Added plugins for GPS

--- scripts/teleop_key.py
+++ scripts/teleop_key.py
@@ -89,7 +89,7 @@ if __name__=="__main__":
     settings = termios.tcgetattr(sys.stdin)
     
     rospy.init_node('teleop_key')
-    pub = rospy.Publisher('vikram/cmd_vel', Twist, queue_size=5)
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=5)
 
     x = 0
     th = 0
